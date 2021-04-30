e201fa8aea3c834e5b0200fb27d6cd22ffd4b1c2
Li Jixiang <jixiang.li@razer.com>
2021-04-30 18:53:40 +0800

    xstate: init xstate helloworld.

--- frontend/xstate/reference.md
+++ frontend/xstate/reference.md
@@ -1,6 +1,9 @@
 ## Source  
 https://github.com/davidkpiano/xstate  
 
+## Env  
+https://vitejs.dev/guide/#scaffolding-your-first-vite-project   
+
 
 ## Template
 https://codesandbox.io/s/xstate-react-typescript-template-wjdvn  
--- frontend/xstate/src/index.tsx
+++ frontend/xstate/src/index.tsx
@@ -1,45 +1,171 @@
 import "./style.css";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { createMachine, assign } from "xstate";
+import { createMachine, assign, send, Machine, spawn, interpret } from "xstate";
 import { useMachine } from "@xstate/react";
 
-interface ToggleContext {
-  count: number;
-}
 
-const toggleMachine = createMachine<ToggleContext>({
-  id: "toggle",
-  initial: "inactive",
-  context: {
-    count: 0
-  },
-  states: {
-    inactive: {
-      on: { TOGGLE: "active" }
+const holdMachine = Machine(
+    {
+      id: "hold",
+      type: "parallel",
+      states: {
+        clutch: {
+          initial: "inactive",
+          states: {
+            active: {
+              entry: ["clutchKeyDown"],
+              on: {
+                clutch_off: "inactive",
+              },
+            },
+            inactive: {
+              entry: ["clutchKeyUp"],
+              on: {
+                clutch_on: "active",
+              },
+            },
+          },
+        },
+        otfs: {
+          initial: "inactive",
+          states: {
+            active: {
+              on: {
+                otfs_off: "inactive",
+              },
+            },
+            inactive: {
+              on: {
+                otfs_on: "active",
+              },
+            },
+          },
+        },
+      },
     },
-    active: {
-      entry: assign({ count: (ctx) => ctx.count + 1 }),
-      on: { TOGGLE: "inactive" }
+    {
+      actions: {
+        clutchKeyDown: (context, event) => {
+          console.log("clutch key down inner");
+        },
+        clutchKeyUp: (context, event) => {
+          console.log("clutch key up inner");
+        },
+      },
     }
-  }
-});
+  );
+  const deviceStateMachine = Machine(
+    {
+      id: "device",
+      initial: "starting",
+      context: {
+        holdMachine: null,
+      },
+      states: {
+        starting: {
+          entry: assign<any>({
+            holdMachine: () => spawn(holdMachine),
+          }),
+          always: 'idle',
+        },
+        idle: {
+          on: {
+            dpiUI: [
+              {
+                target: "dpiUI",
+                cond: { type: "dpiUIGuard" },
+              },
+              { target: ".invalid" },
+            ],
+            clutch: {
+              target: "clutch",
+            },
+          },
+          initial: "normal",
+          states: {
+            normal: {},
+            invalid: {
+              entry: ["notifyFail"]
+            },
+          },
+        },
+        clutch: {
+          initial: 'processing',
+          on: {
+            key_down: {
+              actions: send("clutch_on", {
+                to: (context: any) =>  {
+                    console.log(context);
+                    return context.holdMachine
+                },
+              }),
+              target: '.completed'
+            },
+            key_up: {
+              actions: send("clutch_off", {
+                to: (context: any) => context.holdMachine,
+              }),
+              target: '.completed'
+            },
+          },
+          states: {
+            processing: {},
+            completed: {
+              type: "final",
+            },
+          },
+          onDone: {
+            target: "idle",
+            actions: ["notifyDone"],
+          },
+        },
+        dpiUI: {
+          entry: ["processDPI"],
+          onDone: "idle",
+        },
+      },
+    },
+    {
+      actions: {
+        notifyFail: (context, event) => {
+          console.log("fail");
+        },
+        notifyDone: (context, event) => {
+          console.log("event:", event.type,"context:", context);
+        },
+        processDPI: (context, event) => {
+          console.log("process dpi");
+        },
+      },
+      guards: {
+        dpiUIGuard: (context: any, event: any) => {
+          const { clutch, otfs } = context.holdMachine.state.value;
+          if (clutch === "active" || otfs === "active") {
+            return false;
+          }
+          return true;
+        },
+      },
+    }
+  );
 
 function App() {
-  const [current, send] = useMachine(toggleMachine);
+  const [current, send] = useMachine(deviceStateMachine);
   const active = current.matches("active");
-  const { count } = current.context;
+  const { dpi } = current.context;
+
+  const service = interpret(deviceStateMachine)
+  .onTransition((state) => console.log(state.value, state.context))
+  .start();
 
   return (
     <div className="App">
       <h1>XState React Template</h1>
-      <h2>Fork this template!</h2>
-      <button onClick={() => send("TOGGLE")}>
-        Click me ({active ? "✅" : "❌"})
-      </button>{" "}
-      <code>
-        Toggled <strong>{count}</strong> times
-      </code>
+
+     <button onClick={() => service.send(['clutch', 'key_down'])}>
+        Go clutch
+      </button>
     </div>
   );
 }
